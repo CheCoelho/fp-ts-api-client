bad55e12e4cb4595c3c9d3604c8896eee89f27ad
CheCoelho <checoelho@gmail.com>
2023-01-05 16:22:33 +0200

    feat: axios & TaskEither

--- api-client.ts
+++ api-client.ts
@@ -3,53 +3,17 @@ import { pipe } from "fp-ts/lib/function";
 import * as A from "fp-ts/lib/Array";
 import * as O from "fp-ts/lib/Option";
 import * as R from "fp-ts/lib/Record";
-import * as E from "fp-ts/lib/Either";
-
-export interface APIOptions {
-  HOST: "https://dummyjson.com/";
-}
-
-//MOVE TO TYPES
-export interface EmptyTokenData {
-  access: null;
-  refresh: null;
-}
-export interface TokenData {
-  access: string;
-  refresh: string;
-}
-type ItemID = string; //Create a type that extends all possible ids
-type RouteOptions = "products";
-type ApiMethod = "GET" | "POST";
-type Params = Record<ParamsKey, ItemID>;
-type ParamsKey = "product_id";
+import axios, { AxiosResponse } from "axios";
+import { APIOptions, Params, RouteOptions, ItemID } from "./types";
 
 const _createBaseUrl = (options: APIOptions): string => `${options.HOST}`;
-
-const _createRequestInit = (method: ApiMethod): RequestInit => {
-  return {
-    method: method,
-    mode: "cors" as RequestMode,
-    headers: {
-      "Content-type": "application/json",
-    } as HeadersInit,
-  };
+const config = {
+  headers: {
+    "Content-type": "application/json",
+  },
 };
 
-// const __constructUrlParams = (params: Params): string => {
-//   const keys = Object.keys(params);
-//   let urlParams: string = "";
-
-//   console.log("RECEIVED PARAMS: ", params);
-
-//   keys.map((k) => {
-//     urlParams = urlParams + `${k}=${params[k]}&`;
-//   });
-//   console.log("constructed url params: ", urlParams);
-//   return urlParams;
-// };
-
-const _constructUrlParams = (params: Params) =>
+const _constructUrlParams = (params: Params): string =>
   pipe(
     params,
     R.keys,
@@ -57,104 +21,30 @@ const _constructUrlParams = (params: Params) =>
     A.reduce("", (acc, curr) => acc + curr)
   );
 
-const _constructUrl = (
+export const _constructUrl = (
   route: RouteOptions,
   options: APIOptions,
   id: O.Option<ItemID>,
   params: O.Option<Params>
-): string =>
-  pipe(
-    O.fromNullable(id),
-    O.map((i) => i + "/"),
-    O.getOrElse(() => ""),
-    (idPath) =>
-      pipe(
-        params,
-        O.map((p) => "?" + _constructUrlParams(p)),
-        O.getOrElse(() => ""),
-        (urlParams) =>
-          _createBaseUrl(options) + "/" + route + "/" + idPath + urlParams
-      )
-  );
-// const __constructUrl = (
-//   route: RouteOptions,
-//   options: APIOptions,
-//   id: ItemID | null,
-//   params: Params | null
-// ) => {
-//   let urlParams: string;
-//   let idPath: string;
-//   const baseUrl = _createBaseUrl(options);
-//   if (id) {
-//     idPath = id + "/";
-//   } else {
-//     idPath = "";
-//   }
-//   if (params) {
-//     urlParams = "?" + _constructUrlParams(params);
-//   } else {
-//     urlParams = "";
-//   }
-//   const url = baseUrl + "/" + route + "/" + idPath + urlParams;
-//   console.log("URL CONSTRUCTED: ", url);
-//   return url;
-// };
-
-//Keep these functions here and add request specific routes
-export const makeRequest = async (
-  method: ApiMethod,
-  route: RouteOptions,
-  apiOptions: APIOptions,
-  params: Params | null = null,
-  id: ItemID | null = null
-) => {
-  console.log(
-    `Making Request to ${route} with ${id ? "ID" : "filter params"} ${
-      id ? id : params?.product_id
-    }`
-  );
-  try {
-    const requestInit = _createRequestInit(method);
-    const url = _constructUrl(
-      route,
-      apiOptions,
-      O.fromNullable(id),
-      O.fromNullable(params)
-    );
-    console.log("Making Request: ", url, requestInit);
-    const response = await fetch(url, requestInit);
-    console.log("RESPONSE: ", response);
-    return response;
-  } catch (error) {
-    console.error(error);
-    return error as Error;
-  }
+): string => {
+  const idPath = O.fold(
+    () => "",
+    (i) => `/${i}`
+  )(id);
+  const urlParams = O.fold(
+    () => "",
+    (p: Params) => `?${_constructUrlParams(p)}`
+  )(params);
+  return `${_createBaseUrl(options)}${route}${idPath}${urlParams}`;
 };
 
-export const _makeRequest = (
-  method: ApiMethod,
+export const get = (
   route: RouteOptions,
-  apiOptions: APIOptions,
-  params: Params | null = null,
-  id: ItemID | null = null
-) =>
-  pipe(
-    TE.tryCatch(
-      () =>
-        fetch(
-          _constructUrl(
-            route,
-            apiOptions,
-            O.fromNullable(id),
-            O.fromNullable(params)
-          ),
-          _createRequestInit(method)
-        ),
-      (error) => TE.left(new Error(error as string))
-    )
-    // TE.chain((response) =>
-    //   TE.fromEither(
-    //     TE.map((responseStatus) => new Error(responseStatus), response.ok)
-    //   )
-    // )
+  options: APIOptions,
+  id: O.Option<ItemID>,
+  params: O.Option<Params>
+): TE.TaskEither<Error, AxiosResponse> =>
+  TE.tryCatch(
+    () => axios.get(_constructUrl(route, options, id, params), config),
+    (error) => new Error(error as string)
   );
