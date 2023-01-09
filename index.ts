8f600018e93d3f46ee490d30986eea80a9cec0f1
CheCoelho <checoelho@gmail.com>
2023-01-09 15:15:30 +0200

    feat: invoke fns

--- index.ts
+++ index.ts
@@ -1,34 +1,61 @@
 import * as O from "fp-ts/lib/Option";
 import * as TE from "fp-ts/lib/TaskEither";
 import { pipe } from "fp-ts/lib/function";
-import { _constructUrl, get } from "./api-client";
-import { apiOptions } from "./constants";
+import { _get, _post, _delete } from "./api-client";
+import { apiOptions, postExample } from "./constants";
+import { ItemID, Params, PostExample } from "./types";
+
+export const getProduct = async (id: ItemID) => {
+  console.log("Making a get request for a single product");
+  const rawRes = pipe(
+    _get("products", apiOptions, O.some(id), O.none),
+    TE.chain((response) => TE.right(response.data))
+  )();
+
+  console.log(await rawRes);
+};
+
+export const getProductList = async (params: Params) => {
+  console.log("Making a get request for a list of products");
+
+  const rawRes = pipe(
+    _get("products", apiOptions, O.none, O.some(params)),
+    TE.chain((response) => TE.right(response.data))
+  )();
+  console.log(await rawRes);
+};
+
+export const createProduct = async (data: PostExample) => {
+  console.log("Making a post request to create a product");
+
+  const rawRes = pipe(
+    _post("products/add", apiOptions, data),
+    TE.chain((response) => TE.right(response.data))
+  )();
+  console.log(await rawRes);
+};
+
+export const deleteProduct = async (id: ItemID) => {
+  console.log("Making a delete request to a single product");
+
+  const rawRes = pipe(
+    _delete("products", apiOptions, O.some(id)),
+    TE.chain((response) => TE.right(response.data))
+  )();
+
+  console.log(await rawRes);
+};
 
 const main = async () => {
-  console.log("Running ");
-  //The following 10 lines of code are purely for logging purposes
-  console.log(
-    "Making a get request to URL: ",
-    // _constructUrl("products", apiOptions, O.some("1"), O.none)
-    _constructUrl(
-      "products",
-      apiOptions,
-      O.none,
-      O.some({ category: "smartphones" })
-    )
-  );
-  const rawRes = pipe(
-    //Set the route and the produce ID below. The last argument to the
-    //_constructUrl function is for query param
-
-    //To get a product with a specific ID
-    get("products", apiOptions, O.some("p"), O.none),
-
-    //To get a list of products that matches the query params
-    // get("products", apiOptions, O.none, O.some({ category: "smartphones" })),
-    TE.chain((response) => TE.right(response.data))
-  );
-  console.log("Printing data from response: ", await rawRes());
+  try {
+    await getProduct("1");
+    await createProduct(postExample);
+    await getProduct("100");
+    await getProductList({ category: "smartphones" });
+    await deleteProduct("100");
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 main();
