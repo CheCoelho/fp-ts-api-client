f28c02346f914d431e4c75ee2c55eddb9a12c970
CheCoelho <checoelho@gmail.com>
2023-01-09 15:16:07 +0200

    chore: types

--- types.ts
+++ types.ts
@@ -1,5 +1,5 @@
-import { category } from "fp-ts";
 import "lib.dom.d.ts";
+import { _get, _post } from "./api-client";
 
 export interface APIOptions {
   HOST: string;
@@ -15,9 +15,9 @@ export interface TokenData {
 }
 export type ItemID = string; //Create a export type that extends all possible ids
 export type Category = "smartphones";
-export type RouteOptions = "products";
+export type RouteOptions = "products" | "products/add";
 export type ApiMethod = "GET" | "POST";
-export type Params = Record<ParamsKey, Category>;
+export type Params = { [key: string]: Category };
 export type ParamsKey = "category";
 
 export type RequestInit = {
@@ -29,3 +29,22 @@ export type RequestInit = {
 export type RequestMode = "same-origin" | "no-cors" | "cors";
 
 export type HeadersInit = Headers | string[][] | Record<string, string>;
+
+export type Method = "GET" | "POST";
+
+export interface ApiOptions {
+  HOST: string;
+}
+
+export interface PostExample {
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}
